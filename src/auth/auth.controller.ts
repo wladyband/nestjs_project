4f3492d8cf9b55b25c411f8b63c70aded97e2450
Wladimir Bandeira <wladimir.bandeira@capgemini.com>
2021-07-30 09:41:19 -0300

    Primeiro registro no banco de dados docker

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 
 @Controller()
 export class AuthController {
 
+    constructor(private userService: UserService){}
+
     @Post('register')
-    async register() {
-        return 'register';
+    async register(@Body() body) {
+        return this.userService.create(body);
     }
+
+     
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,11 +7,13 @@ import { User } from './models/user.entity';
 export class UserService {
     constructor(
         @InjectRepository(User) private readonly userRepository: Repository<User>
-    ) {
-        
-    }
+    ) {}
 
     async all(): Promise<User[]> {
-        return await this.userRepository.find();
+        return this.userRepository.find();
+    }
+
+    async create(data): Promise<User> {
+        return this.userRepository.save(data);
     }
 }
