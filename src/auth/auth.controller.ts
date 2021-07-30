b53483815e9312603dbf1f0bbbc4150e438cca35
Wladimir Bandeira <wladimir.bandeira@capgemini.com>
2021-07-30 13:52:36 -0300

    configurando validações do cadastro de usuários

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcryptjs';
+import { RegisterDto } from './models/RegisterDto';
 
 @Controller()
 export class AuthController {
@@ -8,7 +9,12 @@ export class AuthController {
     constructor(private userService: UserService){}
 
     @Post('register')
-    async register(@Body() body) {
+    async register(@Body() body: RegisterDto) {
+
+        if (body.password !== body.password_confirm) {
+            throw new BadRequestException('Passwords do not match!');
+        }
+
         const hashed = await bcrypt.hash(body.password, 12);
         return this.userService.create({
             first_name: body.first_name,
