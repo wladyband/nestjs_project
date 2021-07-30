1ea2fb72dd0b55a844523df5f71099b7b3c877f9
Wladimir Bandeira <wladimir.bandeira@capgemini.com>
2021-07-30 08:43:55 -0300

    iniciando a implementação do auth

--- dist/app.module.js
+++ dist/app.module.js
@@ -12,6 +12,7 @@ const typeorm_1 = require("@nestjs/typeorm");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 const user_module_1 = require("./user/user.module");
+const auth_module_1 = require("./auth/auth.module");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
@@ -28,6 +29,7 @@ AppModule = __decorate([
                 autoLoadEntities: true,
                 synchronize: true,
             }),
+            auth_module_1.AuthModule,
         ],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Post } from '@nestjs/common';
+
+@Controller()
+export class AuthController {
+
+    @Post('register')
+    async register() {
+        return 'register';
+    }
+}
